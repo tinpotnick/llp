e615661df5e15bfd35941f6f5018b1fc7258b12b
Nick Knight <nick@tinpot.co.uk>
2025-01-03 15:03:18 +0000

    user flashcards

--- lib/screens/flashcard_deck.dart
+++ lib/screens/flashcard_deck.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
 import 'package:provider/provider.dart';
-import '../providers/flashcard_provider.dart';
+import '../providers/usercard_provider.dart';
 
-import 'dart:math' as math;
+import '../widgets/flashcard_view.dart';
 
 import 'flashcard_editor.dart';
 
@@ -19,32 +18,44 @@ class _FlashcardDeckScreenState extends State<FlashcardDeckScreen> {
       appBar: AppBar(
         title: Text('Flashcard Deck'),
       ),
-      body: Consumer<FlashcardProvider>(
-        builder: (context, flashcardProvider, child) {
-          final flashcards = flashcardProvider.cards;
+      body: Consumer<UserCardProvider>(
+        builder: (context, usercardProvider, child) {
+          final userflashcards = usercardProvider.userCards;
+          final flashcardList = userflashcards.values.toList();
 
-          if (flashcards.isEmpty) {
+          if (userflashcards.isEmpty) {
             return Center(child: Text('No flashcards available.'));
           }
 
           return ListView.builder(
-            itemCount: flashcards.length,
+            itemCount: userflashcards.length,
             itemBuilder: (context, index) {
+              final flashcard = flashcardList[index];
               return FlashcardTile(
-                flashcard: flashcards[index],
+                userCardProvider: usercardProvider,
+                flashcard: flashcard,
                 onDelete: () {
-                  flashcardProvider.removeCard(index);
+                  usercardProvider.removeCard(flashcard);
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(content: Text('Flashcard deleted.')),
                   );
                 },
                 onEdit: () {
+                  final flshcard =
+                      usercardProvider.getFlashcardForUserCard(flashcard);
+                  if (flshcard == null) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Flashcard not found.')),
+                    );
+                    return;
+                  }
+
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (context) => FlashcardEditorScreen(
                         index: index,
-                        flashcard: flashcards[index],
+                        flashcard: flshcard,
                       ),
                     ),
                   );
@@ -53,190 +64,6 @@ class _FlashcardDeckScreenState extends State<FlashcardDeckScreen> {
             },
           );
         },
-      ),
-    );
-  }
-}
-
-class FlashcardTile extends StatefulWidget {
-  final Flashcard flashcard;
-  final VoidCallback onDelete;
-  final VoidCallback onEdit;
-
-  const FlashcardTile({
-    required this.flashcard,
-    required this.onDelete,
-    required this.onEdit,
-  });
-
-  @override
-  _FlashcardTileState createState() => _FlashcardTileState();
-}
-
-class _FlashcardTileState extends State<FlashcardTile> {
-  final AudioPlayer _audioPlayer = AudioPlayer();
-  bool _isPlaying = false;
-  Duration _currentPosition = Duration.zero;
-  bool _isRevealed =
-      false; // Controls whether the translation and buttons are shown
-
-  Future<void> _playPause() async {
-    if (_isPlaying) {
-      await _audioPlayer.pause();
-    } else {
-      await _audioPlayer.play(UrlSource(widget.flashcard.audioUrl));
-      _audioPlayer.seek(widget.flashcard.start);
-    }
-
-    setState(() {
-      _isPlaying = !_isPlaying;
-    });
-
-    _audioPlayer.onPositionChanged.listen((position) {
-      if (position >= widget.flashcard.end) {
-        _audioPlayer.pause();
-        setState(() {
-          _isPlaying = false;
-          _currentPosition = widget.flashcard.start;
-        });
-      } else {
-        setState(() {
-          _currentPosition = position;
-        });
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _audioPlayer.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final snippetDuration = widget.flashcard.end - widget.flashcard.start;
-
-    // Ensure slider value is within range
-    final sliderValue = math.max(
-      0,
-      math.min(
-        (_currentPosition - widget.flashcard.start).inMilliseconds.toDouble(),
-        snippetDuration.inMilliseconds.toDouble(),
-      ),
-    );
-
-    return Card(
-      margin: const EdgeInsets.all(8.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              widget.flashcard.text,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-            ),
-            SizedBox(height: 8),
-
-            // Playback controls
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Slider(
-                  value: sliderValue.toDouble(),
-                  min: 0,
-                  max: snippetDuration.inMilliseconds.toDouble(),
-                  onChanged: (_) {}, // Slider is read-only for now
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                      icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
-                      onPressed: _playPause,
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 8),
-
-            // View button and revealed content
-            if (!_isRevealed)
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    _isRevealed = true;
-                  });
-                },
-                child: Text('View'),
-              )
-            else
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Translation: ${widget.flashcard.translation}'),
-                  SizedBox(height: 8),
-                  OverflowBar(
-                    children: [
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('Graded as Hard')),
-                          );
-                        },
-                        icon: Icon(Icons.thumb_down),
-                        label: Text('+1m'),
-                      ),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('Graded as Good')),
-                          );
-                        },
-                        icon: Icon(Icons.thumbs_up_down),
-                        label: Text('+2d'),
-                      ),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('Graded as Easy')),
-                          );
-                        },
-                        icon: Icon(Icons.thumb_up),
-                        label: Text('+2d'),
-                      ),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(content: Text('Graded as Easy')),
-                          );
-                        },
-                        icon: Icon(Icons.sentiment_very_satisfied),
-                        label: Text('+4d'),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      ElevatedButton.icon(
-                        onPressed: widget.onEdit,
-                        icon: Icon(Icons.edit),
-                        label: Text('Edit'),
-                      ),
-                      ElevatedButton.icon(
-                        onPressed: widget.onDelete,
-                        icon: Icon(Icons.delete),
-                        label: Text('Delete'),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-          ],
-        ),
       ),
     );
   }
