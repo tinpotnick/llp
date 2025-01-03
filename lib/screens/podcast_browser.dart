d941ec262689fcec70d6aaec57f71afac04228cb
Nick Knight <nick@tinpot.co.uk>
2025-01-03 15:04:03 +0000

    use service

--- lib/screens/podcast_browser.dart
+++ lib/screens/podcast_browser.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'podcast_detail_screen.dart';
+
+import '../services/podcast_service.dart';
 
 class PodcastBrowserScreen extends StatefulWidget {
   @override
@@ -18,22 +18,18 @@ class _PodcastBrowserScreenState extends State<PodcastBrowserScreen> {
       _isLoading = true;
     });
 
-    final url =
-        Uri.parse('https://itunes.apple.com/search?term=$query&media=podcast');
-    final response = await http.get(url);
-
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
+    try {
+      final pdcsts = await PodcastService.fetchPodcastList(query);
       setState(() {
-        _podcasts = data['results'];
+        _podcasts = pdcsts;
         _isLoading = false;
       });
-    } else {
+    } catch (e) {
       setState(() {
         _isLoading = false;
       });
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to fetch podcasts')),
+        SnackBar(content: Text('Failed to fetch podcasts: $e')),
       );
     }
   }
@@ -74,24 +70,75 @@ class _PodcastBrowserScreenState extends State<PodcastBrowserScreen> {
                   itemCount: _podcasts.length,
                   itemBuilder: (context, index) {
                     final podcast = _podcasts[index];
-                    return ListTile(
-                      leading: podcast['artworkUrl100'] != null
-                          ? Image.network(podcast['artworkUrl100'])
-                          : Icon(Icons.library_music),
-                      title:
-                          Text(podcast['collectionName'] ?? 'Unknown Podcast'),
-                      subtitle: Text(podcast['artistName'] ?? 'Unknown Artist'),
-                      onTap: () {
-                        // Navigate to PodcastDetailScreen
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) =>
-                                PodcastDetailScreen(podcast: podcast),
-                          ),
-                        );
-                      },
-                    );
+                    return FutureBuilder(
+                        future: PodcastService.fetchPodcastDetails(podcast),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return ListTile(
+                              leading: CircularProgressIndicator(),
+                              title: Text('Loading...'),
+                              subtitle: Text('Please wait'),
+                            );
+                          } else if (snapshot.hasError) {
+                            return ListTile(
+                              leading: Icon(Icons.error, size: 40.0),
+                              title: Text('Error loading podcast'),
+                              subtitle: Text('${snapshot.error}'),
+                            );
+                          } else if (snapshot.hasData) {
+                            return ListTile(
+                                leading: snapshot.hasData &&
+                                        snapshot.data?.imageUrl != null
+                                    ? Image.network(snapshot.data!.imageUrl)
+                                    : Icon(Icons.place, size: 40.0),
+                                title: Text(podcast.collectionName ??
+                                    'Unknown Podcast'),
+                                subtitle: Text(
+                                    podcast.artistName ?? 'Unknown Artist'),
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => FutureBuilder(
+                                        future:
+                                            PodcastService.fetchPodcastDetails(
+                                                _podcasts[index]),
+                                        builder: (context, snapshot) {
+                                          if (snapshot.connectionState ==
+                                              ConnectionState.waiting) {
+                                            // Show a loading indicator while the data is being fetched
+                                            return Center(
+                                                child:
+                                                    CircularProgressIndicator());
+                                          } else if (snapshot.hasError) {
+                                            // Handle any errors
+                                            return Center(
+                                                child: Text(
+                                                    'Error: ${snapshot.error}'));
+                                          } else if (snapshot.hasData) {
+                                            // Pass the fetched podcast to the PodcastDetailScreen
+                                            return PodcastDetailScreen(
+                                                podcast: snapshot.data);
+                                          } else {
+                                            // Handle the case where there's no data
+                                            return Center(
+                                                child: Text('No data found'));
+                                          }
+                                        },
+                                      ),
+                                    ),
+                                  );
+                                });
+                          } else {
+                            return ListTile(
+                              leading: Icon(Icons.info, size: 40.0),
+                              title: Text('No data available'),
+                              subtitle:
+                                  Text('Podcast details could not be found'),
+                            );
+                          }
+                        });
                   },
                 ),
               ),
