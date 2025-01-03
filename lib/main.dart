52e63d80f392ae32c6414636b95412c8e526b6f0
Nick Knight <nick@tinpot.co.uk>
2025-01-03 15:02:09 +0000

    add other providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/flashcard_provider.dart';
+import 'providers/podcast_provider.dart';
+import 'providers/usercard_provider.dart';
 import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final flashcardProvider = FlashcardProvider();
-  await flashcardProvider.loadFromStorage(); // Load saved flashcards
+  final podcastProvider = PodcastProvider();
+  final usercardProvider = UserCardProvider(flashcardProvider);
+  await flashcardProvider.loadFromStorage();
+  await podcastProvider.loadFromStorage();
+  await usercardProvider.loadFromStorage();
 
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => flashcardProvider),
+        ChangeNotifierProvider(create: (_) => podcastProvider),
+        ChangeNotifierProvider(create: (_) => usercardProvider),
       ],
       child: LanguageLearningApp(),
     ),
@@ -23,7 +31,8 @@ class LanguageLearningApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Language Learning Podcast',
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      theme:
+          ThemeData(primarySwatch: Colors.deepOrange, fontFamily: 'NotoSans'),
       home: HomeScreen(),
     );
   }
